d18998ffbc82d941421aeec40fbdb9f2855a64a4
Gabriele De Rosa <derosagabriele@outlook.it>
2017-02-09 01:37:06 +0100

    Bug fixed
    Fix update bug.

--- update.sh
+++ update.sh
@@ -3,7 +3,7 @@
 echo "Gabux > Update"
 
 # Dependencies
-sudo apt intall curl # curl is required to verify version
+sudo apt install curl # curl is required to verify version
 sudo apt install jq # jq is required to verify version
 sudo apt install git # git is required to download update file
 
@@ -11,7 +11,7 @@ sudo apt install git # git is required to download update file
 LAST=$(curl -s 'https://raw.githubusercontent.com/derogab/gabux/master/version.json' | jq -r '.version')
 CURRENT=$(jq -r '.version' /usr/share/gabux/version.json)
 
-if [[ $LAST != $CURRENT ]]; then
+if [ $LAST != $CURRENT ]; then
   # Update Gabux
 
   # Download gabux from github
