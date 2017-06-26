31d2000609e74bc796732a29533b31cc13f5ba05
Gabriele De Rosa <derosagabriele@outlook.it>
2017-06-27 00:06:57 +0200

    Bug fixed
    Fix bug in linux-update command.

--- commands/linux-update/linux-update.sh
+++ commands/linux-update/linux-update.sh
@@ -1,12 +1,7 @@
 #! /bin/bash
-
 echo "Gabux > Update"
-
-sudo apt update;
 IFS=';' read updates security_updates < <(/usr/lib/update-notifier/apt-check 2>&1);
-#echo $updates;
-#echo $security_updates;
-if (( updates == 0 )); then
+if (( $updates == 0 )); then
     echo "No updates are available."
 else
     echo "There are updates available: $updates updates; $security_updates security updates."
@@ -14,5 +9,4 @@ else
     sudo apt upgrade;
     sudo apt autoremove;
 fi
-
 echo "The system is updated.";
