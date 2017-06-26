d119c5c92735fcffd1c4350ae8b6aebd2d62d3f4
Gabriele De Rosa <derosagabriele@outlook.it>
2017-06-26 23:43:45 +0200

    Update a command
    Update a command: linux-update.

--- commands/linux-update/linux-update.sh
+++ commands/linux-update/linux-update.sh
@@ -2,7 +2,17 @@
 
 echo "Gabux > Update"
 
-sudo apt update
-sudo apt upgrade
+sudo apt update;
+IFS=';' read updates security_updates < <(/usr/lib/update-notifier/apt-check 2>&1);
+#echo $updates;
+#echo $security_updates;
+if (( updates == 0 )); then
+    echo "No updates are available."
+else
+    echo "There are updates available: $updates updates; $security_updates security updates."
+    sudo apt update;
+    sudo apt upgrade;
+    sudo apt autoremove;
+fi
 
-echo "Completed."
+echo "The system is updated.";
