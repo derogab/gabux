dda7abe1896659a768a596b1862f518a65fbbe80
Gabriele De Rosa <derosagabriele@outlook.it>
2017-07-10 00:02:18 +0200

    Uninstall script converted

---
+++ uninstall.py
@@ -0,0 +1,15 @@
+import os
+import getpass
+
+print('Gabux started by '+getpass.getuser()+'.')
+print('Removing gabux..')
+
+# Remove gabux folder
+print('Deleting gabux folder..')
+out = os.popen('sudo rm -r /usr/share/gabux').read()
+# Remove gabux bash command
+print('Removing gabux bash command..')
+out = os.popen('sudo rm -r /usr/bin/gabux').read()
+
+print('Gabux successfully removed.')
+print('Today is a sad day.')
