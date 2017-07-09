dd35a3561a51395ad7aa97861c8a2f5055eb6f07
Gabriele De Rosa <derosagabriele@outlook.it>
2017-07-09 19:05:54 +0200

    Added uninstall
    Added command to uninstall gabux. Even though no one will use it.

---
+++ uninstall.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+echo "Removing gabux.."
+# Remove gabux
+echo "Removing gabux folder.."
+sudo rm -r /usr/share/gabux # remove gabux folder
+echo "Removing gabux bash command.."
+sudo rm -r /usr/bin/gabux # remove gabux command
+echo "Gabux successfully removed."
+echo "Today is a sad day."
