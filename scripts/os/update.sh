eacee916659acebc76c0c4ff5e1927afb469aaaf
Gabriele De Rosa <derosagabriele@outlook.it>
2018-09-05 21:24:44 +0200

    Bug fixed in OS update script

--- scripts/os/update.sh
+++ scripts/os/update.sh
@@ -1,13 +1,6 @@
 #! /bin/bash
-echo "Checking updates for your OS..";
-IFS=';' read updates security_updates < <(/usr/lib/update-notifier/apt-check 2>&1);
-if (( $updates == 0 )); then
-    echo "No updates are available.";
-else
-    echo "There are updates available: $updates updates; $security_updates security updates.";
-    sudo apt-get update -y;
-    sudo apt-get upgrade -y;
-    sudo apt-get autoremove -y;
-fi
-echo "The system is updated.";
+echo "Updating your OS..";
+sudo apt-get update -y;
+sudo apt-get upgrade -y;
+sudo apt-get autoremove -y;
 exit;
